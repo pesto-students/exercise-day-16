451d433f18798245500d060218286b4329f92041
Nikhil Arora <narora200@gmail.com>
2018-06-26 04:00:26 +0530

    WIP: React App partial done

--- Exercises/01/src/index.js
+++ Exercises/01/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom'; // eslint-disable-line
+
 import App from './App';
+import PostContainer from './components/PostContainer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/post/:id" component={PostContainer} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
