2ea8eb363b757f6d57847b2d63ca984a28bc8889
Nikhil Arora <narora200@gmail.com>
2018-06-26 05:28:29 +0530

    WIP

--- question-1/script.js
+++ question-1/script.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-if (localStorage['logged-in'] === true) {
+if (localStorage.getItem('logged-in') === 'true') {
 	console.log("The user is logged in!");
 } else {
 	console.log("The user is not logged in!");
