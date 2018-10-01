f5b3b3eda127402e9b12beac139697dc02d6e72a
vipulrawat <vipulrawat.imi@live.in>
2018-10-02 00:41:30 +0530

    [WIP]-Q modified

--- Problems/src/Queue.js
+++ Problems/src/Queue.js
@@ -1,40 +1,57 @@
-class Queue {
-  constructor(){
-    this.state = {
-      const arr : [],
-      let front = -1,
-      let rear = -1,
-    }
+module.exports = class Queue {
+  constructor() {
+    //  this.state = {
+    this.arr = [];
+    //  }
   }
-  isEmpty = () => {
-    let { arr } = this.state;
-    if(arr.length === 0) {
+  isEmpty() {
+    //  let { arr } = this.state;
+    if (this.arr.length === 0) {
       return true;
     }
     return false;
   }
 
-  enqueue = (element) => {
-    let {arr, front, rear} = this.state;
-    if(!this.isEmpty()) {
-      if (front === -1) {
-        front = 0;
-      }
-      rear += 1;
-      arr[rear] = element;
-      console.log(element+' inserted');
+  enqueue(element) {
+    //  let {arr, front, rear} = this.state;
+    //  if(!this.isEmpty()) {
+    //  console.log('inin')
+    //  if (this.front === -1) {
+    //  this.front = 0;
+    //  }
+    //  this.rear += 1;
+    // this.arr[this.rear] = element;
+    //  console.log(element+' inserted');
+    //  }
+    this.arr.push(element);
+  }
+
+  dequeue() {
+    // let {arr, front, rear} = this.state;
+    //  let deleted = this.arr[this.front];
+    //  this.front += 1;
+    //  console.log(deleted + ' is deleted');
+    //  return deleted;
+    if (this.arr.length === 0) {
+      return null;
     }
+    return this.arr.splice(0, 1)[0];
   }
 
-  dequeue = () => {
-    let {arr, front, rear} = this.state;
-    let deleted = arr[front];
-    front += 1;
-    console.log(deleted + ' is deleted');
-    return deleted;
+  peek() {
+    if (this.arr.length === 0) {
+      return null;
+    }
+    return this.arr[0];
   }
-
-  peek = () => {
-    return this.state.arr[0];
+  toString() {
+    return this.arr.toString();
   }
-}
+  // eslint-disable-next-line
+  toString(cb) {
+    let str = '';
+    // eslint-disable-next-line
+    this.arr.forEach(el => str = str + cb(el) + ',');
+    return str;
+  }
+};
