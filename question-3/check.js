e72f45e2981173bdcca250dff92bf678e519ab70
vipulrawat <vipulrawat.imi@live.in>
2018-10-02 03:50:28 +0530

    [WIP] Segment 2

--- question-3/check.js
+++ question-3/check.js
@@ -1,6 +1,11 @@
 /* eslint-disable */
 let value = '';
 
+const keyElement = document.getElementById('key');
+const valueElement = document.getElementById('value');
+const list = document.getElementById('list');
+document.getElementById("form").addEventListener("submit", handleSubmit);
+
 window.setInterval(() => {
   if (value === '' || value === null) {
     value = sessionStorage.getItem('world');
@@ -8,4 +13,20 @@ window.setInterval(() => {
     document.getElementsByClassName('status')[0].innerText = 'Value add successful';
   }
 }, 200);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  sessionStorage.setItem(keyElement.value, valueElement.value);
+  // alert('Saved:'+sessionStorage.getItem(keyElement.value));
+  list.innerHTML += `<li><strong>${keyElement.value}</strong>:${valueElement.value}<span class="close">---X---</span></li>`;
+  // --------------------------
+  var close = document.getElementsByClassName("close");
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      sessionStorage.removeItem(keyElement.value);
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
 
